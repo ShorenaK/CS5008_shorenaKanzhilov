d426fe92dbb9152e5219267c84fd8dfe86cfc32c
ShorenaK <shorenaanzhilov@gmail.com>
2024-09-06 17:32:36 -0400

    doubl arrays and char arrays  way to initilase the arrays

--- Exmaples/example3.c
+++ Exmaples/example3.c
@@ -4,6 +4,11 @@
 
 int main(){
 	int array[3] = {1, 2, 3,};
+	double array2[3] = {1.5, 2.3, 4.5};
+	char array3[6] = {'h', 'e', 'l', 'l', 'o', '\0'};
+	char array4[] = "Hello World!";
+	printf("%s  %s\n", array3, array4);
+	
 	/*array[0] = 0;
 	array[1] = 1;
 	array[2] = 2;
