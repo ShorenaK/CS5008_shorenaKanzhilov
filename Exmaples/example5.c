4a20e8dc5067f0ecc29e20cb61bccb19810aca39
ShorenaK <shorenaanzhilov@gmail.com>
2024-09-08 17:02:58 -0400

    helper function for print

--- Exmaples/example5.c
+++ Exmaples/example5.c
@@ -12,12 +12,18 @@ struct student makeStudent(int id, int age, double creditHours){
 	tempStudent.creditHours = creditHours;
 	return tempStudent;
 }
+
+void printStudent(struct student aStudent){
+	printf("Student: %d %d %f\n", aStudent.id, aStudent.age, aStudent.creditHours);
+
+}
 int main(){
 	struct student s1 = makeStudent(1001, 49, 14.5);
 	struct student s2 = makeStudent(1011, 29, 17.9);
 
-	printf("Student: %d, %d, %f\n", s1.id, s1.age, s1.creditHours);
-	printf("Student: %d, %d, %f\n", s2.id, s2.age, s2.creditHours);
+	printStudent(s1);
+	printStudent(s2);
+
 	return 0;
 
 
