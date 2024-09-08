ac6000b0cc0078eeeebf2b6a10b7a8845e380c5d
ShorenaK <shorenaanzhilov@gmail.com>
2024-09-08 17:19:01 -0400

    pointer for the varable  updated

--- Exmaples/example6.c
+++ Exmaples/example6.c
@@ -6,8 +6,9 @@ void update(int argument){
 
 int main(){
 	int value = 5;
+	int* p_value = &value;
 	printf("Value is: %d\n", value);
-	printf("The address of value is: %p\n", &value);
+	printf("The address of value is: %p\n", p_value);
 	update(value);
 	printf("Value is: %d\n", value);
 	return 0;
