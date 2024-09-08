ffc0636a3e1e401c37a7cbcd74cbb11f8f3e9abf
ShorenaK <shorenaanzhilov@gmail.com>
2024-09-08 16:50:00 -0400

    helper functin

--- Exmaples/example5.c
+++ Exmaples/example5.c
@@ -5,16 +5,16 @@ struct student{
 	double creditHours;
 };
 
+struct student makeStudent(int id, int age, double creditHours){
+	struct student tempStudent;
+	tempStudent.id = id;
+	tempStudent.age = age;
+	tempStudent.creditHours = creditHours;
+	return tempStudent;
+}
 int main(){
-	struct student s1;
-	s1.id = 1001;
-	s1.age = 49;
-	s1.creditHours = 14.5;
-
-	struct student s2;
-	s2.id = 1011;
-	s2.age = 29;
-	s2.creditHours = 16.7;
+	struct student s1 = makeStudent(1001, 49, 14.5);
+	struct student s2 = makeStudent(1011, 29, 17.9);
 
 	printf("Student: %d, %d, %f\n", s1.id, s1.age, s1.creditHours);
 	printf("Student: %d, %d, %f\n", s2.id, s2.age, s2.creditHours);
