9ed2680d45d5b6f1b2aa36b12dca62e00b28048b
ShorenaK <shorenaanzhilov@gmail.com>
2024-09-09 13:24:42 -0400

    final version of function pointers reffren

--- examples/example9.c
+++ examples/example9.c
@@ -10,20 +10,25 @@ void printStudent(student_t* aStudent){
 	printf("Student ID: %d, Age: %d\n",(*aStudent).id, (*aStudent).age);
 
 }
+
+void updateStudent(student_t* aStudent, int age, int id){
+	(*aStudent).age = age;
+	(*aStudent).id = id;
+}
+
 int main(){
         student_t  s1;
         student_t  s2;
 
-	//Lets assign some values to s1 and s2;
-        
-	s1.id = 1001;
-	s1.age = 49;
-
-	s2.id = 1002;
-	s2.age = 56;
+	updateStudent(&s1, 49, 1011);
+	updateStudent(&s2, 56, 1012);
 
 
         printStudent(&s1);
 	printStudent(&s2);
+
+	updateStudent(&s2, s2.age, 1013);
+	printStudent(&s2);
+
         return 0;
 }
